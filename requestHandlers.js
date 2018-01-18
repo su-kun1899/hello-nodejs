ecc1fa14db3c221a73db8ba63bff05a340157844
su-kun1899 <higedrum.coz@gmail.com>
2018-01-18 16:31:19 +0900

    送信したデータを表示する

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require("child_process").exec;
+var queryString = require("querystring");
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'
@@ -20,10 +20,10 @@ function start(response) {
   response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("Hello Upload");
+  response.write("You've sent the text: " + queryString.parse(postData).text);
   response.end();
 }
 
