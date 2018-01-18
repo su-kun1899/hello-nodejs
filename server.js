d30ce367d622b06f1fd5a5c4827fb0ea015d6d3c
su-kun1899 <higedrum.coz@gmail.com>
2018-01-18 16:25:26 +0900

    requestをログに出力させる

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " recieved.");
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    var postData = "";
+    request.addListener("data", function (postDataChunk) {
+      postData += postDataChunk;
+      console.log("Recieved POST data chunk '" + postDataChunk + "'.");
+    });
+
+    request.addListener("end", function () {
+      route(handle, pathname, response, postData);
+    })
   }
 
   http.createServer(onRequest).listen(8888);
