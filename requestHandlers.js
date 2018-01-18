e7dcebf84b335c8a8a64c0a7b079435e0a72f708
su-kun1899 <higedrum.coz@gmail.com>
2018-01-18 12:53:16 +0900

    ノンブロッキングに書き換えてみたけど、ブラウザに結果が表示されないのでうれしくない

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,14 +1,15 @@
+var exec = require("child_process").exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
 
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
+  var content = "empty";
 
-  sleep(10000);
+  exec("ls -lah", function (err, stdout, stderr) {
+    content = stdout;
+  });
 
-  return "Hello Start";
+  return content;
 }
 
 function upload() {
